7479e3f583b2cc21f12255048a1a2f38d045ffb8
AnishTalaviya123 <anishtalaviya853@gmail.com>
2023-01-27 21:09:10 +0530

    print input in console

--- app.js
+++ app.js
@@ -3,14 +3,32 @@ var elem = document.getElementById('timer')
 var timeId;
 var input = document.querySelector('.text-input');
 var a = 0;
-var tryAgainBtn = document.querySelector('.myBtn')
+var tryAgainBtn = document.querySelector('.myBtn');
 
+var typingText = document.querySelector('.defultPara');
+var myParagraph = typingText.textContent.split("");
+// console.log(myParagraph);
+typingText.textContent = "";
+
+// to split the input value
+
+
+// to get input value 
 input.addEventListener('keypress', function newFun() {
+    var inputChar = input.value;
+    console.log(inputChar);
     if (a < 1) {
         timeId = setInterval(myFun, 1000);
     }
     a = 1;
 })
+
+// input.addEventListener('keypress', tryFun)
+// function tryFun() {
+    
+    // var inputChar = input.value;
+    // console.log(inputChar);
+// }
 
 function myFun() {
     if (timeLeft == -1) {
@@ -24,10 +42,17 @@ function myFun() {
     }
 }
 
-tryAgainBtn.addEventListener('click',function toBtn(){
-    if (timeLeft < 61) {
-        clearInterval(timeId);
-        elem.innerHTML = "Time:60s";
-        input.value = "";
-    }   
+// to stop the timer
+tryAgainBtn.addEventListener('click', function toBtn() {
+    clearInterval(timeId);
+    elem.innerHTML = "Time:60s";
+    input.value = "";
 })
+
+// split paragraphs
+myParagraph.forEach((myParagraph) => {
+    typingText.innerHTML += '<span class="separateText">' + myParagraph + '</span>';
+})
+
+// var inputChar = input.value;
+//     console.log(inputChar);
